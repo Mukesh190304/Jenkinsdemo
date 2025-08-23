3d9797ba9c86b220cb08e641e6d85140fbb24651
Mukesh190304 <mukesh190304@gmail.com>
2025-08-23 12:03:18 +0530

    FirstCommit

---
+++ Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CalculatorApp
+
+{
+
+    public class Calculator
+
+    {
+
+        public int add(int a, int b)
+
+        {
+
+            return a + b;
+
+        }
+
+        public int product(int a, int b)
+
+        {
+
+            return a * b;
+
+        }
+
+    }
+
+    public class Program
+
+    {
+
+        public static void Main(string[] args)
+
+        {
+
+            Calculator c = new Calculator();
+
+            int add = c.add(1, 2);
+
+            Console.WriteLine("Addition: " + add);
+
+            int product = c.product(1, 2);
+
+            Console.WriteLine("Product: " + product);
+
+        }
+
+    }
+
+}
+
---
+++ CalculatorTesting/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using CalculatorApp;
+
+namespace CalculatorTesting
+
+{
+
+    [TestFixture]
+
+    public class Tests
+
+    {
+
+        private Calculator _calculator;
+
+        [SetUp]
+
+        public void Setup()
+
+        {
+
+            _calculator = new Calculator();
+
+        }
+
+        [Test]
+
+        public void Add_TwoNumbers_Equal()
+
+        {
+
+            int result = _calculator.add(10, 2);
+
+            Assert.AreEqual(12, result);
+
+        }
+
+        // This test should use a different expected value
+
+        [Test]
+
+        public void Add_TwoNumbers_NotEqual()
+
+        {
+
+            int result = _calculator.add(10, 2);
+
+            Assert.AreNotEqual(15, result); // 12 ≠ 15, so this will pass
+
+        }
+
+        [Test]
+
+        public void Product_TwoNumbers_Equal()
+
+        {
+
+            int result = _calculator.product(4, 5);
+
+            Assert.AreEqual(20, result);
+
+        }
+
+        // Again, use a different expected value
+
+        [Test]
+
+        public void Product_TwoNumbers_NotEqual()
+
+        {
+
+            int result = _calculator.product(4, 5);
+
+            Assert.AreNotEqual(25, result); // 20 ≠ 25, so this will pass
+
+        }
+
+    }
+
+}
+
